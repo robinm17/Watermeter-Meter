006c156608e4584eb52c166ac3f1164ffba1445d
robinm17 <43373858+robinm17@users.noreply.github.com>
2023-01-15 23:26:10 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,168 @@
+import numpy as np
+import torch
+import torchvision
+import matplotlib.pyplot as plt
+from time import time
+from torchvision import datasets, transforms
+from torch import nn, optim
+
+
+
+transform = transforms.Compose([transforms.ToTensor(),
+                              transforms.Normalize((0.5,), (0.5,)),
+                              ])
+
+trainset = datasets.MNIST('PATH_TO_STORE_TRAINSET', download=True, train=True, transform=transform)
+valset = datasets.MNIST('PATH_TO_STORE_TESTSET', download=True, train=False, transform=transform)
+trainloader = torch.utils.data.DataLoader(trainset, batch_size=64, shuffle=True)
+valloader = torch.utils.data.DataLoader(valset, batch_size=64, shuffle=True)
+
+dataiter = iter(trainloader)
+images, labels = next(dataiter)
+# print(type(images))
+# print(images.shape)
+# print(labels.shape)
+
+# plt.imshow(images[0].numpy().squeeze(), cmap='gray_r')
+# # plt.show()
+
+# figure = plt.figure()
+# num_of_images = 60
+# for index in range(1, num_of_images + 1):
+#     plt.subplot(6, 10, index)
+#     plt.axis('off')
+#     plt.imshow(images[index].numpy().squeeze(), cmap='gray_r')
+# plt.show()
+
+# Layer details for the neural network
+input_size = 784
+hidden_sizes = [128, 64]
+output_size = 10
+
+# Build a feed-forward network
+model = nn.Sequential(nn.Linear(input_size, hidden_sizes[0]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[0], hidden_sizes[1]),
+                      nn.ReLU(),
+                      nn.Linear(hidden_sizes[1], output_size),
+                      nn.LogSoftmax(dim=1))
+# print(model)
+
+optimizer = optim.SGD(model.parameters(), lr=0.01, momentum=0.5)
+
+criterion = nn.NLLLoss()
+images, labels = next(iter(trainloader))
+images = images.view(images.shape[0], -1)
+
+logps = model(images)
+loss = criterion(logps, labels)
+     
+     
+print('Before backward pass: \n', model[0].weight.grad)
+
+loss.backward()
+
+print('After backward pass: \n', model[0].weight.grad)
+
+print('Initial weights - ', model[0].weight)
+
+images, labels = next(iter(trainloader))
+images.resize_(64, 784)
+
+# Clear the gradients, do this because gradients are accumulated
+optimizer.zero_grad()
+
+# Forward pass, then backward pass, then update weights
+output = model(images)
+loss = criterion(output, labels)
+loss.backward()
+print('Gradient -', model[0].weight.grad)
+
+
+optimizer.step()
+print('Updated weights - ', model[0].weight)
+
+
+optimizer = optim.SGD(model.parameters(), lr=0.003, momentum=0.9)
+time0 = time()
+epochs = 15
+for e in range(epochs):
+    running_loss = 0
+    for images, labels in trainloader:
+        # Flatten MNIST images into a 784 long vector
+        images = images.view(images.shape[0], -1)
+    
+        # Training pass
+        optimizer.zero_grad()
+        
+        output = model(images)
+        loss = criterion(output, labels)
+        
+        #This is where the model learns by backpropagating
+        loss.backward()
+        
+        #And optimizes its weights here
+        optimizer.step()
+        
+        running_loss += loss.item()
+    else:
+        print("Epoch {} - Training loss: {}".format(e, running_loss/len(trainloader)))
+print("\nTraining Time (in minutes) =",(time()-time0)/60)
+
+
+########################################################################################################
+
+
+
+def view_classify(img, ps):
+    ''' Function for viewing an image and it's predicted classes.
+    '''
+    ps = ps.data.numpy().squeeze()
+
+    fig, (ax1, ax2) = plt.subplots(figsize=(6,9), ncols=2)
+    ax1.imshow(img.resize_(1, 28, 28).numpy().squeeze())
+    ax1.axis('off')
+    ax2.barh(np.arange(10), ps)
+    ax2.set_aspect(0.1)
+    ax2.set_yticks(np.arange(10))
+    ax2.set_yticklabels(np.arange(10))
+    ax2.set_title('Class Probability')
+    ax2.set_xlim(0, 1.1)
+    plt.tight_layout()
+    
+
+images, labels = next(iter(valloader))
+
+img = images[0].view(1, 784)
+# Turn off gradients to speed up this part
+with torch.no_grad():
+    logps = model(img)
+
+# Output of the network are log-probabilities, need to take exponential for probabilities
+ps = torch.exp(logps)
+probab = list(ps.numpy()[0])
+print("Predicted Digit =", probab.index(max(probab)))
+view_classify(img.view(1, 28, 28), ps)
+
+plt.show()
+
+
+correct_count, all_count = 0, 0
+for images,labels in valloader:
+  for i in range(len(labels)):
+    img = images[i].view(1, 784)
+    # Turn off gradients to speed up this part
+    with torch.no_grad():
+        logps = model(img)
+
+    # Output of the network are log-probabilities, need to take exponential for probabilities
+    ps = torch.exp(logps)
+    probab = list(ps.numpy()[0])
+    pred_label = probab.index(max(probab))
+    true_label = labels.numpy()[i]
+    if(true_label == pred_label):
+      correct_count += 1
+    all_count += 1
+
+print("Number Of Images Tested =", all_count)
+print("\nModel Accuracy =", (correct_count/all_count))
